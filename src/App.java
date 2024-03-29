d382beb63f0fbd124d9ee8a6e7d1330f5a65b934
wjsdud132 <wjsdud132@naver.com>
2024-03-29 15:31:57 +0900

    스캐너를 if문으로 바꾸기

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("월을 입력하세요. : ");
+        int month = scanner.nextInt();
+
+        if(3<=month && month <= 5) {
+            System.out.println("봄 입니다.");
+        }
+        else if(6<=month && month <= 8) {
+            System.out.println("여름 입니다.");
+        }     
+        else if(9<=month && month <= 11) {
+            System.out.println("가을 입니다.");
+        }
+        else{
+            System.out.println("겨울 입니다.");
+        }
+        scanner.close();
     }
 }
